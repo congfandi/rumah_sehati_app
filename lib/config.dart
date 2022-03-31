fe2359c36b3d5a60cd66d453c8ea839652efe38e
congfandi <congfandi@gmail.com>
2022-04-01 06:26:11 +0700

    [FIX] Release 1 April 2022

--- lib/config.dart
+++ lib/config.dart
@@ -10,8 +10,9 @@ class Environments {
 }
 
 class ConfigEnvironments {
-  static String get _currentEnvironments => Environments.LOCAL;
-      // kDebugMode ? Environments.LOCAL : Environments.PRODUCTION;
+  static String get _currentEnvironments => Environments.PRODUCTION;
+
+  // kDebugMode ? Environments.LOCAL : Environments.PRODUCTION;
   static final List<Map<String, String>> _availableEnvironments = [
     {
       'env': Environments.LOCAL,
